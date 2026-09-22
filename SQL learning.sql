3f82eb93a3ce63b1a1002adf4c4912d1b50e75b3
Pranav Gurle <137593348+Pranvg@users.noreply.github.com>
2026-09-22 21:22:49 +0530

    Create SQL learning.sql

---
+++ SQL learning.sql
@@ -0,0 +1,40 @@
+USE instagram;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT PRIMARY KEY,
+    age INT CHECK (age >= 13),
+    name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) UNIQUE,
+    followers INT DEFAULT 0,
+    following INT
+);
+
+INSERT INTO users
+(id, age, name, email)
+VALUES
+(10, 10, "random", "bob@yahoo.in");
+
+
+
+INSERT INTO users
+(id, age, name, email, followers, following)
+VALUES
+(1, 14, "adam", "adam@yahoo.in", 123, 145),
+(2, 15, "bob", "bob@yahoo.in", 200, 200),
+(3, 16, "casey", "casey@yahoo.in", 300, 306),
+(4, 17, "pranav", "pranav@yahoo.in", 200, 105);
+
+#SELECT DISTINCT age FROM users; 
+
+SELECT name, followers
+FROM users
+WHERE age < 16;
+
+
+CREATE TABLE IF NOT EXISTS post (
+    id INT PRIMARY KEY,
+    content VARCHAR(100),
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
